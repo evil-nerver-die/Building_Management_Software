0cc63c62f44b95d60c6d3706accbd3aed0bc249d
Omegaduc <51270223+Omegaduc@users.noreply.github.com>
2022-01-24 15:11:54 +0700

    update

--- src/main/java/com/bms/persistences/CustomerRepository.java
+++ src/main/java/com/bms/persistences/CustomerRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
     List<Customer> getByCustomerNameContaining(String name);
