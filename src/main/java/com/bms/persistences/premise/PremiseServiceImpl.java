fb7ee4854c5316ed72bd08d75726c970e06a584c
evil-never-die <longlehako@gmail.com>
2022-02-11 00:45:04 +0700

    Premise's base func

---
+++ src/main/java/com/bms/persistences/premise/PremiseServiceImpl.java
@@ -0,0 +1,35 @@
+package com.bms.persistences.premise;
+
+import com.bms.models.Premises;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+
+public class PremiseServiceImpl implements PremiseService {
+
+    PremiseRepository repository;
+
+    public PremiseServiceImpl(PremiseRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public LinkedList<Premises> getByPremiseName(String premiseName) {
+        return repository.getByPremiseName(premiseName);
+    }
+
+    @Override
+    public Iterable<Premises> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Premises save(Premises premises) {
+        return repository.save(premises);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        repository.deleteById(id);
+    }
+}
