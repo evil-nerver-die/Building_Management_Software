3c619c6e7bb90a17d9583a423ccda621af7afb2a
evil-never-die <longlehako@gmail.com>
2022-03-28 16:27:17 +0700

    fix getter

--- src/main/java/com/bms/DTO/ContractDto.java
+++ src/main/java/com/bms/DTO/ContractDto.java
@@ -2,18 +2,22 @@ package com.bms.DTO;
 
 import com.bms.models.Contract;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class ContractDto {
+    private Integer conId;
+
     String conName;
 
     String conCode;
 
     String conCreated;
 
-    public ContractDto(Contract contract){
-        this.conName = contract.getConName();
-        this.conCode = contract.getConCode();
-        this.conCreated = contract.getConCreated();
-    }
+//    public ContractDto(Contract contract){
+//        this.conName = contract.getConName();
+//        this.conCode = contract.getConCode();
+//        this.conCreated = contract.getConCreated();
+//    }
 }
