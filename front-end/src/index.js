68029683b4886f38c537ced21cfabd0b82268a8e
Omegaduc <taduc1610@gmail.com>
2022-04-14 10:22:12 +0700

    Revert "add customer scene"
    This reverts commit 5991bc8fa4ab7c2f1e8e9a4104dcf48cd5ef5d85, reversing
    changes made to edbc5f39f7b372c73f8c33b42f9a14653e45006e.

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -7,7 +7,6 @@ import Home from './scene/home';
 import Premise from './scene/premise';
 import Customer from './scene/customer';
 import Account from './scene/account';
-import Contract from './scene/contract';
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -21,7 +20,7 @@ ReactDOM.render(
 					<Route path="/service" element={<App />} />
 					<Route path="/customer" element={<Customer />} />
 					<Route path="/employee" element={<App />} />
-					<Route path="/account" element={<App />} />
+					<Route path="/account" element={<Account />} />
 				</Route>
 			</Routes>
 		</HashRouter>
--- front-end/src/scene/customer/index.jsx
+++ front-end/src/scene/customer/index.jsx
@@ -1,55 +1,10 @@
 import './index.css';
 
 import React from 'react';
-import { Table, Tag, Space } from 'antd';
-
-const { Columns } = Table;
-
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    dob: 32,
-    phone: 'New York No. 1 Lake Park',
-    email: 'lol',
-    gender: true,
-  },
-  {
-    key: '2',
-    name: 'Duc Beo',
-    dob: 32,
-    phone: 'New York No. 1 Lake Park',
-    email: 'lol',
-    gender: true,
-  },
-  {
-    key: '3',
-    name: 'Mia Kalifax',
-    dob: 32,
-    phone: 'New York No. 1 Lake Park',
-    email: 'lol',
-    gender: false,
-  },
-];
 
 const Customer = () => {
   return (
-    <React.Fragment>
-      <Table dataSource={data}>
-            <Columns title="Name" dataIndex="name" key="name" />
-            <Columns title="Date of birth" dataIndex="dob" key="dob" />
-            <Columns 
-                title="Gender" 
-                dataIndex="name" 
-                key="name"
-                render={(gender)=>(
-                    <>{gender === true ? 'Male':'Female'}</>
-                )}
-            />
-            <Columns title="Phone" dataIndex="phone" key="phone" />
-            <Columns title="Email" dataIndex="email" key="email" />
-      </Table>
-    </React.Fragment>
+    <div>Customer</div>
   )
 }
 
