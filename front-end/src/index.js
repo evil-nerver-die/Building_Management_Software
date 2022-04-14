edbc5f39f7b372c73f8c33b42f9a14653e45006e
Omegaduc <taduc1610@gmail.com>
2022-04-14 09:44:04 +0700

    Revert "Merge pull request #18 from evil-nerver-die/layout/customer"
    This reverts commit 42074a039e6b4242bf627d8e8403ed35d468a67a, reversing
    changes made to 10769c5c8530af37a6eb35d6e7aa8bb696ebdce9.

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,7 +6,6 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './scene/home';
 import Premise from './scene/premise';
 import Customer from './scene/customer';
-import Contract from './scene/contract';
 import Account from './scene/account';
 
 ReactDOM.render(
@@ -17,7 +16,7 @@ ReactDOM.render(
 					{/* Đổi tên element component sau khi code xong scene */}
 					<Route path="/home" element={<Home />} />
 					<Route path="/premise" element={<Premise />} />
-					<Route path="/contract" element={<Contract />} />
+					<Route path="/contract" element={<App />} />
 					<Route path="/service" element={<App />} />
 					<Route path="/customer" element={<Customer />} />
 					<Route path="/employee" element={<App />} />
