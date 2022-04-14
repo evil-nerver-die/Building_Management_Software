c9b90da9afba0aaaf215909935ef77aa81daeb21
evil-never-die <longlehako@gmail.com>
2022-04-14 13:56:35 +0700

    Contract scene update

--- front-end/src/scene/contract/components/editContract.jsx
+++ front-end/src/scene/contract/components/editContract.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
+import {Form, Input} from 'antd';
 import './selectedContract.css';
-const editContract = prop => {
+
+const { TextArea } = Input;
+
+const EditContract = prop => {
+	const [form] = Form.useForm();
+	const onFinish = value => {
+		console.log(value);
+	};
 	return (
-		<div className="container">
-			<div className="name">Contract's name:</div>
-			<div className="name-value">{prop.contract.name}</div>
-			<div className="code">Code:</div>
-			<div className="code-value">{prop.contract.code}</div>
-			<div className="dateEnd">Date Ended:</div>
-			<div className="ended-value">{prop.contract.dateEnd}</div>
-			<div className="provider">Provider:</div>
-			<div className="provider-value">{prop.contract.provider}</div>
-			<div className="des">Description:</div>
-			<div className="des-value">{prop.contract.des}</div>
+		<div>
+			<Form form={form} onFinish={onFinish}>
+				<Form.Item name="name" label="Contract's name">
+					<Input defaultValue={prop.name}/>
+				</Form.Item>
+				<Form.Item name="code" label="Code">
+					<Input defaultValue={prop.code}/>
+				</Form.Item>
+				<Form.Item name="dateEnd" label="Date Ended">
+					<Input defaultValue={prop.dateEnd}/>
+				</Form.Item>
+				<Form.Item name="provider" label="Provide">
+					<Input defaultValue={prop.provider}/>
+				</Form.Item>
+				<Form.Item name="des" label="Description:">
+					<TextArea rows={4} defaultValue={prop.des} />
+				</Form.Item>
+			</Form>
 		</div>
 	);
 };
 
-export default editContract;
+export default EditContract;
