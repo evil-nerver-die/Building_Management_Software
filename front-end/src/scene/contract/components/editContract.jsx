a7671f90d40dfbb56213e89b6a1c54eef40654fc
King of Noobs <54358291+evil-nerver-die@users.noreply.github.com>
2022-04-30 14:50:37 +0700

    add component func into Contract sence

--- front-end/src/scene/contract/components/editContract.jsx
+++ front-end/src/scene/contract/components/editContract.jsx
@@ -1,35 +1,61 @@
 import React from 'react';
 import {Form, Input} from 'antd';
 import './selectedContract.css';
+import { stores } from '../../../store/storeInitializer';
 
 const { TextArea } = Input;
 
-const EditContract = prop => {
-	const [form] = Form.useForm();
-	const onFinish = value => {
+export default class EditContract extends React.Component {
+	constructor(prop) {
+		super(prop);
+		this.state = {
+			isLoad: false
+		};
+	}
+
+	data = stores.contractStore.contractSelected;
+
+	componentDidUpdate(prevProps) {
+		if (this.props.id !== prevProps.id) {
+			this.data = stores.contractStore.contractSelected;
+			this.setState({ isLoad: !this.state.isLoad });
+		}
+	}
+
+	onFinish = value => {
 		console.log(value);
 	};
-	return (
-		<div>
-			<Form form={form} onFinish={onFinish}>
-				<Form.Item name="name" label="Contract's name">
-					<Input defaultValue={prop.name}/>
-				</Form.Item>
-				<Form.Item name="code" label="Code">
-					<Input defaultValue={prop.code}/>
-				</Form.Item>
-				<Form.Item name="dateEnd" label="Date Ended">
-					<Input defaultValue={prop.dateEnd}/>
-				</Form.Item>
-				<Form.Item name="provider" label="Provide">
-					<Input defaultValue={prop.provider}/>
-				</Form.Item>
-				<Form.Item name="des" label="Description:">
-					<TextArea rows={4} defaultValue={prop.des} />
-				</Form.Item>
-			</Form>
-		</div>
-	);
-};
 
-export default EditContract;
+	render() {
+		return(
+			<div>
+				<Form
+					onFinish={this.onFinish}
+					initialValues={{
+						name: this.data.name,
+						code: this.data.code,
+						ended: this.data.ended,
+						provider: this.data.provider,
+						des: this.data.des
+					}}
+				>
+					<Form.Item name="name" label="Tên hợp đồng">
+						<Input />
+					</Form.Item>
+					<Form.Item name="code" label="Code">
+						<Input />
+					</Form.Item>
+					<Form.Item name="dateEnd" label="Ngày hết hạn">
+						<Input />
+					</Form.Item>
+					<Form.Item name="provider" label="Nhà cung cấp">
+						<Input />
+					</Form.Item>
+					<Form.Item name="des" label="Thông tin chi tiết:">
+						<TextArea rows={4} />
+					</Form.Item>
+				</Form>
+			</div>
+		);
+	}
+}
