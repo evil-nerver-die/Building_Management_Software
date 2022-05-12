6bbfbc2f690c89dda1232cbf38da355802c41a84
King of Noobs <54358291+evil-nerver-die@users.noreply.github.com>
2022-05-12 15:52:14 +0700

    add Port

--- src/main/java/com/bms/controllers/PremisesApiController.java
+++ src/main/java/com/bms/controllers/PremisesApiController.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "http://localhost:3000/", maxAge = 3600)
 @RestController
 @RequestMapping("/api/premises")
 public class PremisesApiController {
