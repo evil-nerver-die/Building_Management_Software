ef78ec04a75bed3962324af2a50ee1ffa0c1e741
DA Ngo <danhngo1102@gmail.com>
2022-05-27 01:16:23 +0700

    update footer

--- front-end/src/common/footer/footer.jsx
+++ front-end/src/common/footer/footer.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import { Row, Col, Image } from 'antd';
+import './footer.css';
 
-const AppFooter = () => {
-	return <div>Tòa nhà Đức béo - Số 10B, ngõ 126 Hoàng Văn Thái - Thanh Xuân - Hà Nội.</div>;
-};
-
-export default AppFooter;
+export default class AppFooter extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div>
+					<Row justify="space-between">
+						<Col span={8}>
+							<img src={require('./logo.png')}></img>
+						</Col>
+						<Col span={8}>
+							<label className="label1">THÔNG TIN TÒA NHÀ</label>
+							<Row>
+								<label>Tòa nhà Đức béo</label>
+							</Row>
+							<Row>
+								<label>Cho thuê văn phòng toà nhà tiêu chuẩn 5 tầng nổi, hiện đại, độc lập.</label>
+							</Row>
+							<Row>
+								<label>Cung cấp các dịch vụ cao cấp như lắp đặt, sửa chữa, cung cấp với chất lượng tuyệt vời</label>
+							</Row>
+							<Row>
+								<label>Địa chỉ: số 10B phố Hoàng Văn Thái, Thanh Xuân, Hà Nội</label>
+							</Row>
+							<Row>
+								<label>Liên hệ: 0962694093 (anh Đức béo)</label>
+							</Row>
+						</Col>
+						<Col span={8}>
+							<label className="label1">THÔNG TIN NGƯỜI PHÁT TRIỂN</label>
+							<Row>
+								<label>Tạ Quang Đức</label>	
+							</Row>
+							<Row>
+								<label>Liên hệ: fb.com/duc.ta.14473</label>
+							</Row>
+							<Row>
+								<label>SĐT: 0962694093</label>
+							</Row>
+						</Col>
+					</Row>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
